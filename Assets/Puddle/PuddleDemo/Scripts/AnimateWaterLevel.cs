253cb007affe4ed2f049e43f95c822bda9450512
Tay Hao Cheng <42330555+Chindianese@users.noreply.github.com>
2022-11-27 18:05:58 +0800

    Animate water level

--- Assets/Puddle/PuddleDemo/Scripts/AnimateWaterLevel.cs
+++ Assets/Puddle/PuddleDemo/Scripts/AnimateWaterLevel.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class AnimateWaterLevel : MonoBehaviour
 {
+    [SerializeField]
+    [Range(0,1)]
+    private float _animateSpeed = 0.2f;
+    [Range(0,1)]
+    [SerializeField]
+    private float _maxDampness = 0.2f;
+    [Range(0, 1)]
+    [SerializeField]
+    private float _dampnessOffset = 0.0f;
+    [Range(0,1)]
+    [SerializeField]
+    private float _waterLevelOffset = 0.1f;
+
+    private Material _mat;
+    private float _currentWaterLevel = 0.0f;
+    private float _currentDampness = 0.0f;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _mat = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float sin = (Mathf.Sin(_animateSpeed * Time.time) *(0.5f-_waterLevelOffset) + 0.5f + _waterLevelOffset);
+        float sin2 = (Mathf.Sin(_animateSpeed * Time.time) *(0.5f + _dampnessOffset) + 0.5f+ _dampnessOffset);
+        //Debug.Log(sin);
+        _currentWaterLevel = sin;
+        _currentDampness = Mathf.Min(sin2,_maxDampness);
+        _mat.SetFloat("_WaterHeight", _currentWaterLevel);
+        _mat.SetFloat("_Dampness", _currentDampness);
     }
 }
